81a74610e099fbc37554e5df5176b72b27b59e50
Wojciech Opulski <wojtekopulski@gmail.com>
2020-10-24 19:30:24 +0200

    password validation added

--- client/src/components/LoginForm/validateForm.js
+++ client/src/components/LoginForm/validateForm.js
@@ -10,13 +10,23 @@ export default function validateForm(values) {
 	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
 		errors.email.push("Invalid email address");
   }
-  
+
+  console.log('VALIDATION', values)
   //password validation
   if (!values.password) {
     errors.password.push("Please enter a password");
   } else {
     if(values.password.length < 6) {
        errors.password.push("Password must be at least 6 characteres")
+    } 
+    if(!/(?=.*[a-z])/g.test(values.password)) {
+       errors.password.push("Password must contain at least one small letter")
+    } 
+    if(!/(?=.*[A-Z])/g.test(values.password)) {
+       errors.password.push("Password must contain at least one large letter")
+    } 
+    if(!/(?=.*\d)/g.test(values.password)) {
+       errors.password.push("Password must contain at least one digit")
     } 
     //TODO: at least one small letter | one big letter | one number 
     // if(values.){}
